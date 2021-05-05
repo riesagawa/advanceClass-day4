103ff8622d18e863423e09cd7d2238443baffd1d
riesagawa <75202121+riesagawa@users.noreply.github.com>
2021-05-05 21:53:19 +0900

    Add files via upload

---
+++ products.php
@@ -0,0 +1,27 @@
+<?php
+  $products =[
+    [
+      "name" => "アボカド",
+      "price" => 230,
+      "image" => "./images/image_1.jpg"
+    ],
+    [
+      "name" => "オレンジ",
+      "price" => 100,
+      "image" => "./images/image_2.jpg"
+
+    ],
+    [
+      "name" => "レモン",
+      "price" => 200,
+      "image" => "./images/image_3.jpg",
+
+    ],
+    [
+      "name" => "パイナップル",
+      "price" => 900,
+      "image" => "./images/image_4.jpg",
+
+    ]
+  ];
+?>
